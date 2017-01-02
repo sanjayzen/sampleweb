f392ce059778a0a1bda7302cd2aa52c128f89de9
sanjay.zende <sanjayzen@gmail.com>
2017-01-02 20:14:47 +0200

    another sonar code quality

--- devops/src/main/java/com/devops/sample/SampleSonar.java
+++ devops/src/main/java/com/devops/sample/SampleSonar.java
@@ -4,6 +4,8 @@ public class SampleSonar {
 	
 	public String name;
 	
+	public static String lastName;
+	
 	public void print() {
 		System.out.println("This is sample sonar code quality check");
 	}
