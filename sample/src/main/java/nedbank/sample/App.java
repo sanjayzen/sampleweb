5fd20fe15581b8aa296bb478d5043b30aa54b420
sanjay.zende <sanjayzen@gmail.com>
2017-10-25 21:01:23 +0200

    show me

--- sample/src/main/java/nedbank/sample/App.java
+++ sample/src/main/java/nedbank/sample/App.java
@@ -11,6 +11,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!kk" );
+        System.out.println( "Hello World! show me" );
     }
 }
